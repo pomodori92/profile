2bd77f453f0083d964bda4452c4ae3b4f7233acd
pomodori92 <45762339+pomodori92@users.noreply.github.com>
2024-06-07 22:35:12 +0200

    Update script.js
    Optimized the code.

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
     const subtitleElement = document.querySelector('.subtitle');
-    const textArray = ["Femboy-", "Catboy-", "Developer-","Boykisser-"];
+    const textArray = ["Femboy-", "Catboy-", "Developer-", "Boykisser-"];
     let index = 0;
     let charIndex = 0;
     const typingSpeed = 300;
     const eraseSpeed = 150;
+    const pauseBetweenWords = 1000;
 
-    function animateText() {
+    function updateText() {
         const currentWord = textArray[index];
-        if (charIndex === currentWord.length) {
-            setTimeout(eraseText, eraseSpeed);
-            return;
+        subtitleElement.innerText = currentWord.slice(0, charIndex) + '|';
+
+        if (charIndex < currentWord.length) {
+            charIndex++;
+            setTimeout(updateText, typingSpeed);
+        } else {
+            setTimeout(() => {
+                charIndex--;
+                eraseText();
+            }, pauseBetweenWords);
         }
-        const currentText = currentWord.slice(0, charIndex) + '|';
-        if (currentText !== subtitleElement.innerText) {
-            subtitleElement.innerText = currentText;
-        }
-        charIndex++;
-        setTimeout(animateText, typingSpeed);
     }
 
     function eraseText() {
-        if (charIndex === 0) {
-            index = (index + 1) % textArray.length;
-            setTimeout(animateText, typingSpeed);
-            return;
-        }
         const currentWord = textArray[index];
-        const currentText = currentWord.slice(0, charIndex - 1) + '|';
-        if (currentText !== subtitleElement.innerText) {
-            subtitleElement.innerText = currentText;
+        subtitleElement.innerText = currentWord.slice(0, charIndex) + '|';
+
+        if (charIndex > 0) {
+            charIndex--;
+            setTimeout(eraseText, eraseSpeed);
+        } else {
+            index = (index + 1) % textArray.length;
+            setTimeout(updateText, typingSpeed);
         }
-        charIndex--;
-        setTimeout(eraseText, eraseSpeed);
     }
 
-    animateText();
+    updateText();
 });
